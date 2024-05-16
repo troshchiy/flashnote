40aadcb524caaa2c3254de766e09d031b8c64158
Vladislav Troshchiy <vladislav.troshchiy@gmail.com>
2024-05-16 14:37:24 +0700

    Create models: Note, Question

--- flashnote/editor/models.py
+++ flashnote/editor/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from uuid import uuid4
 
-# Create your models here.
+
+class Note(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+
+    def __str__(self):
+        return f'Note id:{self.id}\nText: "{self.text}"'
+
+
+class Question(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    note = models.OneToOneField(Note, on_delete=models.CASCADE)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='questions')
+
+    def __str__(self):
+        return f'Question id: {self.id}\nNote id: {self.note}\nLabel: "{self.text}"'
