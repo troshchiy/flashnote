f5bff5429e2ac76c7a37b3e3138410d83733b888
Vladislav Troshchiy <vladislav.troshchiy@gmail.com>
2024-05-17 21:56:34 +0700

    Add field 'question' to the model Note

--- flashnote/editor/models.py
+++ flashnote/editor/models.py
@@ -6,6 +6,7 @@ from uuid import uuid4
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     text = models.TextField()
+    question = models.TextField(null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
