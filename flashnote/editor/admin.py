ecd364ae0ac8fb302a26701812c03a4c7e1c8bd9
Vladislav Troshchiy <vladislav.troshchiy@gmail.com>
2024-05-23 21:17:42 +0700

    Add models Notebook, Page

--- flashnote/editor/admin.py
+++ flashnote/editor/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Note
+from .models import Notebook, Page, Note
 
 
+admin.site.register(Notebook)
+admin.site.register(Page)
 admin.site.register(Note)
--- flashnote/editor/models.py
+++ flashnote/editor/models.py
@@ -3,13 +3,33 @@ from django.contrib.auth.models import User
 from uuid import uuid4
 
 
+class Notebook(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notebooks')
+
+    def __str__(self):
+        return f'Notebook {self.title}'
+
+
+class Page(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    notebook = models.ForeignKey(Notebook, related_name='pages', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Page {self.title}'
+
+
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     text = models.TextField()
     question = models.TextField(null=True, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+    page = models.ForeignKey(Page, related_name='notes', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'Note id:{self.id}\nText: "{self.text}"'
+        return f'Note id:{self.id}\nQuestion: "{self.question}"\nText: "{self.text}"'
