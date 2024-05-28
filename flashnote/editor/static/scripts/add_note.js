85ef0312599ac30ea37f324a148f61d40a4195d5
Vladislav Troshchiy <vladislav.troshchiy@gmail.com>
2024-05-28 18:13:28 +0700

    Add the ability to add new notes with shortcut CTRL+ENTER

--- flashnote/editor/static/scripts/add_note.js
+++ flashnote/editor/static/scripts/add_note.js
@@ -1,24 +1,71 @@
 let noteForm = document.querySelectorAll(".note-form")
-let container = document.querySelector("#form-container")
-let addButton = document.querySelector("#add-note-btn")
-let totalForms = document.querySelector("#id_form-TOTAL_FORMS")
-
 let formNum = noteForm.length-1 //Get the number of the last form on the page with zero-based indexing
-addButton.addEventListener('click', addForm)
+let formRegex = RegExp(`form-(\\d){1}-`,'g') //Regex to find all instances of the form number
 
-function addForm(e){
-    e.preventDefault()
-
+function createForm() {
     let newForm = noteForm[0].cloneNode(true) //Clone the note form
-    let formRegex = RegExp(`form-(\\d){1}-`,'g') //Regex to find all instances of the form number
+    let totalForms = document.querySelector("#id_form-TOTAL_FORMS")
 
     formNum++ //Increment the form number
-    newForm.innerHTML = newForm.innerHTML.replace(formRegex, `form-${formNum}-`) //Update the new form to have the correct form number
-    newForm.querySelector(".question").setAttribute('value', "");
+
+    newForm.innerHTML = newForm.innerHTML.replace(formRegex, `form-${formNum}-`)
+    newForm.querySelector(".question").setAttribute("value", "");
     newForm.querySelector(".question").querySelector("textarea").value = "";
     newForm.querySelector(".note").querySelector("textarea").value = "";
+    newForm.querySelector(".question").querySelector("textarea").style.height = "15px";
+    newForm.querySelector(".note").querySelector("textarea").style.height = "15px";
     newForm.querySelector(".order").value = formNum+1;
-    container.appendChild(newForm) //Insert the new form at the end of the list of forms
 
-    totalForms.setAttribute('value', `${formNum+1}`) //Increment the number of total forms in the form management
+    totalForms.setAttribute("value", `${formNum+1}`) //Increment the number of total forms in the form management
+
+    return newForm;
+}
+
+
+function addForm(){
+    event.preventDefault();
+    newForm = createForm();
+
+    let container = document.querySelector("#form-container");
+    container.appendChild(newForm); //Insert the new form at the end of the list of forms
+
+    newForm.querySelector(".note textarea").focus();
+}
+
+document.onkeydown = keydown;
+
+function keydown(){
+  if (event.ctrlKey && event.keyCode==13){ //CTRL+ENTER
+    element = document.activeElement
+    if (element.tagName == "TEXTAREA") {
+            newForm = createForm();
+
+            referenceNode = element.parentNode.parentNode
+            referenceNode.parentNode.insertBefore(newForm, referenceNode.nextSibling);
+
+            order = 1;
+            noteForm.forEach((form) => {
+                form.querySelector("input.order").setAttribute("value", order);
+                order++;
+            });
+        if (element.parentNode.classList.contains("note")) {
+            newForm.querySelector(".note textarea").focus();
+        }
+        else {
+            qTextarea = newForm.querySelector(".question textarea");
+            qTextarea.classList.add("shown");
+            qTextarea.addEventListener("blur", addClassShown);
+            qTextarea.focus();
+        }
+    }
+    else {
+        addForm(event);
+    }
+  }
+}
+
+
+function addClassShown(e) {
+    e.target.classList.remove("shown");
+    e.target.removeEventListener("blur", addClassShown);
 }
