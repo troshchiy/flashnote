4de2f37d88bf771b41e707e3639c354ad6e1bdde
Vladislav Troshchiy <vladislav.troshchiy@gmail.com>
2024-09-09 09:30:49 +0700

    Login now redirects to notebooks list. Add register btn to base template

--- flashnote/flashnote/settings.py
+++ flashnote/flashnote/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'editor:notebooks_list'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
